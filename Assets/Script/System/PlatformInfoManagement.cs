8f18231c4bf3cb512351a950d08bb5cb5092dad3
roy5820 <troy5830@gmail.com>
2024-03-20 14:41:29 +0900

    캐릭터 무브먼트 기능구현 완료

--- Assets/Script/System/PlatformInfoManagement.cs
+++ Assets/Script/System/PlatformInfoManagement.cs
@@ -7,7 +7,7 @@ public class PlatformInfoManagement : MonoBehaviour
 {
     public int indexNum = 0;//해당 플렛폼이 플렛폼 리스트의 몇번째 위치에 있는지 나타내는 인덱스 번호
     public LayerMask characterLayer;//플렛폼위에 올라올 수 있는 캐릭터의 레이어 값
-    private GameObject onPlatformCharacter = null;//해당 플렛폼의 있는 캐릭터 obj
+    public GameObject onPlatformCharacter = null;//해당 플렛폼의 있는 캐릭터 obj
     public Transform standingObj = null;//플렛폼위에 설 위치
 
     //onPlatformCharacter 프로퍼티
@@ -32,10 +32,9 @@ public class PlatformInfoManagement : MonoBehaviour
     {
         
         //캐릭터가 플렛폼에 들어올때 처리
-        if (((1 << collision.gameObject.layer) & characterLayer) != 0 ){
-            Debug.Log("플렛폼에 들어옴");
+        if (((1 << collision.gameObject.layer) & characterLayer) != 0 )
+        {
             onPlatformCharacter = collision.gameObject;//onPlatformCharacter에 들어온 캐릭터 obj값 저장
-            onPlatformCharacter.transform.SetParent(transform);//onPlatformCharacter를 해당 플렛폼의 자식으로 귀속
         }
     }
 
@@ -45,7 +44,6 @@ public class PlatformInfoManagement : MonoBehaviour
         //캐릭터가 플렛폼에 나갈때 처리
         if (((1 << collision.gameObject.layer) & characterLayer) != 0)
         {
-            Debug.Log("플렛폼에 나감");
             onPlatformCharacter = null;//onPlatformCharacter 값 null로 초기화
         }
     }
--- Assets/Script/character/CharacterMovement.cs
+++ Assets/Script/character/CharacterMovement.cs
@@ -10,6 +10,8 @@ public class CharacterMovement : MonoBehaviour
 
     Coroutine moveCoroutine = null;//이동 구현 코루틴 작동 시 저장할 변수
 
+    public LayerMask platformLayer;//플렛폼 레이어 값
+
     private void Start()
     {
         characterRbody = GetComponent<Rigidbody2D>();//리지드 바디 값 초기화
@@ -20,49 +22,42 @@ public class CharacterMovement : MonoBehaviour
     private void Update()
     {
         //이동 테스트를 위한 키입력
-        float getKeyH = Input.GetAxis("Horizontal");//키입력 받기
 
-        if (getKeyH != 0)
-            Debug.Log("이동중 여부: " + moveCoroutine != null);
+        if (Input.GetKeyDown(KeyCode.A))
+            moveCoroutine = StartCoroutine(StraightLineMovement(-1, 10, 1));
 
-        if (getKeyH != 0 && moveCoroutine == null)
-        {
-            Debug.Log("이동 키 입력: " + getKeyH);
-            moveCoroutine = StartCoroutine(StraightLineMovement(getKeyH, 60, 1));
-        }
+        if (Input.GetKeyDown(KeyCode.D))
+            moveCoroutine = StartCoroutine(StraightLineMovement(1, 10, 1));
     }
 
     //이동 구현 코루틴
-    public IEnumerator StraightLineMovement(float moveDirX, float movePower, int moveSpaceDistance)
+    public IEnumerator StraightLineMovement(int moveDirX, float movePower, int moveSpaceDistance)
     {
-        PlatformInfoManagement parentPlatformInfo = null;//플렛폼 정보값을 가져올 변수
-        int platformIndex = -1;//플렛폼 인덱스 값이 저장될 변수
+        PlatformInfoManagement onPlatformInfo = null;//플렛폼 정보값을 가져올 변수
+        int onPlatformIndex = -1;//플렛폼 인덱스 값이 저장될 변수
         //moveSaceNum만큼 칸 이동을 구현하는 반복문
         for (int i = 0; i < moveSpaceDistance; i++)
         {
-            parentPlatformInfo = transform.parent.GetComponent<PlatformInfoManagement>();//자신이 서있는 플렛폼의 정보 컴포넌트를 가져오기
-            platformIndex = parentPlatformInfo.indexNum;//자신이 속한 플렛폼 인덱스 값
-            int moveIndex = platformIndex + (moveDirX > 0 ? 1 : -1);//이동 할 플렛폼의 인데스 값
-            Debug.Log("인덱스 값: "+platformIndex + ", " + moveIndex);
+            RaycastHit2D onPlatform = Physics2D.Raycast(transform.position, Vector2.down, 1, platformLayer);//레이케스트로 현재 플렛폼 정보 가져오기
+            onPlatformInfo = onPlatform.collider.gameObject.GetComponent<PlatformInfoManagement>();//자신이 서있는 플렛폼의 정보 컴포넌트를 가져오기
+            onPlatformIndex = onPlatformInfo.indexNum;//자신이 속한 플렛폼 인덱스 값
+            int moveIndex = onPlatformIndex + moveDirX;//이동 할 플렛폼의 인데스 값
 
             Vector3 targetPlatformPos = getGameManager.GetStandingPos(moveIndex);//이동할 플렛폼 오브젝트 값
+            
             GameObject targetPlatformOnObj = getGameManager.GetOnPlatformObj(moveIndex);//플렛폼 정보값 초기화
 
             //이동 가능 여부 체크
             if (targetPlatformPos != Vector3.zero ? targetPlatformOnObj == null : false)
             {
-                Debug.Log("예외처리 성공: " + transform.position.x + ", " + targetPlatformPos.x);
-                characterCol2D.enabled = false;//이동 시 콜라이더 비활성화
-
                 //실질적인 이동구현을 할 반복문
                 while (moveDirX > 0 ? transform.position.x < targetPlatformPos.x : transform.position.x > targetPlatformPos.x)
                 {
                     characterRbody.velocity = new Vector2(moveDirX * movePower, 0);//이동 구현
-                    Debug.Log(characterRbody.velocity);
                     yield return null;
                 }
                 characterRbody.velocity = Vector3.zero;//이동 종료시 이동 값 zero로 초기화
-                characterCol2D.enabled = false;//이동 종료 시 콜라이더 활성화
+                transform.position = new Vector3(targetPlatformPos.x, transform.position.y, transform.position.z);
             }
             //이동 불가 시 반복문 강제 종료
             else
@@ -70,7 +65,6 @@ public class CharacterMovement : MonoBehaviour
         }
         
         moveCoroutine = null;//코루틴 종료
-        Debug.Log("코루틴 종료: " + (moveCoroutine == null));
         yield return null;
     }
 }
