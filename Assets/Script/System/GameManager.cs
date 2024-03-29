77cac5f36d6c28c3e9d0933ac143ece1d7ab378f
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-03-29 16:33:20 +0900

    싱글톤 패턴 개선

--- Assets/Script/System/GameManager.cs
+++ Assets/Script/System/GameManager.cs
@@ -2,33 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    private static GameManager instance = null;//게임 메니저 인스턴스화를 위한 변수 값
-    
-
     public GameState state;//게임 상태 값이 저장되는 변수
 
 
     public GameObject[] PlatformList;//플랫폼 리스트
-
-    private void Awake()
-    {
-        state = GameState.Start;//게임 시작 시 start상태값으로 초기화
-
-        //인스턴스 초기화
-        if(instance == null)
-            instance = this;
-    }
-
-    //게임 매니저 인스턴스에 접근할 수 있는 프로퍼티
-    public static GameManager Instance
-    {
-        get
-        {
-            return instance;
-        }
-    }
 
     //특정 위치의 플렛폼안에 캐릭터obj의 정보 반환 함수
     public GameObject GetOnPlatformObj(int indexNum)
