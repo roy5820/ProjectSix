278ef8fddaff57742ff40ed92393fab536bcf04d
roy5820 <troy5830@gmail.com>
2024-04-17 17:31:31 +0900

    적 HUD Hp바 구현

--- Assets/Script/UI/HUD/EnemyHUDController.cs
+++ Assets/Script/UI/HUD/EnemyHUDController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHUDController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    EnemyController _eController;//적 컨트롤러
+    public Image hpBar;//체력바 이미지 오브젝트
+
+    private void Start()
     {
-        
+        _eController = transform.parent.GetComponent<EnemyController>();//적 컨트롤러 가져오기
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+        //상태 값에 따른 HUD 없데이트
+        if (_eController)
+        {
+            if (_eController.direction == CharacterDirection.Left)
+                this.transform.localScale = new Vector3(-0.1f, 0.1f, 0.1f);
+            UpdateHealthBar(_eController.NowHp, _eController.maxHp);
+        }
+    }
+
+    void UpdateHealthBar(int currentHealth, int maxHealth)
+    {
+        float healthPercentage = (float)currentHealth / maxHealth;
+        hpBar.fillAmount = healthPercentage;
     }
 }
