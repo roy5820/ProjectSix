7f6190406bcfd7e4530ec34aefc2e9276c530ab7
roy5820 <troy5830@gmail.com>
2024-04-23 16:21:08 +0900

    UI, 애니메이션 등 그래픽 리소스 적용

--- Assets/Script/character/Player/PlayerInputHandler.cs
+++ Assets/Script/character/Player/PlayerInputHandler.cs
@@ -7,6 +7,7 @@ public class PlayerInputHandler : MonoBehaviour
     private GameManager _gameManager;//게임 메니저
     private BattleManager _battleManager;//배틀 메니저
     private PlayerController _playerController;//플레이어 컨트롤러
+    private Animator _playerAni;//플레이어 애니메이션
 
     //입력 키 값
     [Header("Input Key Seting")]
@@ -19,12 +20,13 @@ public class PlayerInputHandler : MonoBehaviour
         _gameManager = GameManager.Instance;//게임 메니저 가져오기
         _battleManager = BattleManager.Instance;//배틀 매니저 가져오기
         _playerController = this.GetComponent<PlayerController>();//플레이어 컨트롤러 초기화
+        _playerAni = this.GetComponent<Animator>();
     }
 
     private void Update()
     {
         //플레이어 턴 체크 및 행동 가능 여부 체크
-        if (_playerController.isTurnReady && _playerController.isAvailabilityOfAction)
+        if (_playerController.isTurnReady && !_playerController.isStatusProcessing)
         {
             
             //이동 키 입력 처리(이동 키 하나로 이동 + 일반 공격 처리)
@@ -39,6 +41,11 @@ public class PlayerInputHandler : MonoBehaviour
                     //이동가능 여부 체크
                     if (_battleManager.GetOnPlatformObj(nexIndex) == null)
                     {
+                        //이동 방향에 따른 isFront 파라미터 변경
+                        if (moveDir == _playerController.direction)
+                            _playerAni.SetBool("IsFront", true);
+                        else
+                            _playerAni.SetBool("IsFront", false);
                         _playerController.isAvailabilityOfAction = false;
                         _playerController.TransitionState(StateEnum.Move, moveDir);
                     }
