72a58bd948bfe5fbe565198dec18afa8052a5f54
roy5820 <troy5830@gmail.com>
2024-05-02 12:53:36 +0900

    캐릭터, 아이템 상태관리하는 스크립터블 오브젝트 수정

--- Assets/Script/Item/UseItem.cs
+++ Assets/Script/Item/UseItem.cs
@@ -7,21 +7,21 @@ public class UseItem : MonoBehaviour
 {
     private PlayerController _playerController;
     public StateEnum useState;//아이템 사용시 사용할 상태
-    public int cost = 5;
+    public int useCost = 5;//사용 시 지불 코스트
 
     private void Start()
     {
-        _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();//플레이어 객체 가져오기
     }
 
     //아이템 사용 처리
     public void ClickItem()
     {
-
-        if(!_playerController.isStatusProcessing && _playerController.isTurnReady && _playerController.nowBattery >= cost)
+        //아이템 사용 가능 여부 체크
+        if(!_playerController.isStatusProcessing && _playerController.isTurnReady && _playerController.nowBattery >= useCost)
         {
-            _playerController.TransitionState(useState);
-            _playerController.nowBattery -= cost;
+            _playerController.TransitionState(useState);//아이템 사용
+            _playerController.nowBattery -= useCost;//코스트 지불
         }
             
     }
