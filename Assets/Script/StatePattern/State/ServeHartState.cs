40b112ecd2ab031635a44f44a180d2b2c1ef9afa
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-05-27 13:09:54 +0900

    서브하트 칩 버그 수정

--- Assets/Script/StatePattern/State/ServeHartState.cs
+++ Assets/Script/StatePattern/State/ServeHartState.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 //서브 하트 칩 기능 구현(죽었다 부활하는 기능)ㅂ
-public class ServeHartState : StateBase
+public class ServeHartState : DieState
 {
     public int serveHartCnt = 1;//부활 횟수
     public float recoveryRate = 0.5f;//부활 시 체력회복 비율
     public float respawnDelay = 0.3f;//리스폰 시 딜레이
+
+    public float deadDelay = 1.2f;//죽을 시 딜레이
+    public string deadAniPara = "DieState";//죽을 시 재생할 애니 파라미터
     protected override IEnumerator StateFuntion(params object[] datas)
     {
         yield return new WaitForSeconds(sateDelayTime);//애니메이션 출력을 위한 딜레이
@@ -18,12 +21,13 @@ public class ServeHartState : StateBase
             serveHartCnt--;//부활 횟수 감소
             characterController._characterStatus.nowHp = (int)(characterController._characterStatus.maxHp * recoveryRate);//체력 회복
             yield return new WaitForSeconds(respawnDelay);//리스폰 시 딜레이 구현
-            yield return base.StateFuntion(datas);
+            characterController.isStatusProcessing = false;//캐릭터 상태 처리 종료
+            yield break;
         }
+        _animator.SetTrigger(deadAniPara);//죽을 시 애니 재생
+        yield return new WaitForSeconds(deadDelay);//죽을 시 딜레이 구현
+
         //불가능 시 죽음 상태 호출
-        else
-        {
-            characterController.TransitionState(StateEnum.Die);
-        }
+        yield return base.StateFuntion(datas);
     }
 }
