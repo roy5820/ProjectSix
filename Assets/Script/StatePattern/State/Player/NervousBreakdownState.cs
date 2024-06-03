5c18220da8f4ae9c09cfcb15c1a04efdee876ffb
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-06-03 12:16:03 +0900

    신경계 폭주 기능 개선 및 보스 체력바 표시

--- Assets/Script/StatePattern/State/Player/NervousBreakdownState.cs
+++ Assets/Script/StatePattern/State/Player/NervousBreakdownState.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class NervousBreakdownState : StateBase
 {
     private Coroutine endCheckCoroutine = null;//
+    private PlayerController playerController = null;
+    //이벤트 등록
+    private void OnEnable()
+    {
+        TurnEventBus.Subscribe(TurnEventType.EnemyTurn, StopBreakdown);
+    }
+
+    //이벤트 해제
+    private void OnDisable()
+    {
+        TurnEventBus.Unsubscribe(TurnEventType.EnemyTurn, StopBreakdown);
+    }
+
     protected override IEnumerator StateFuntion(params object[] datas)
     {
+        playerController = (PlayerController)characterController;//플레이어 컨트롤러 가져오기
         yield return new WaitForSeconds(sateDelayTime);//딜레이 구현
-
-        PlayerController playerController = (PlayerController)characterController;//플레이어 컨트롤러 가져오기
+        
         //폭주 여부에 따른 효과 처리
         //폭주 종료 처리
         if (playerController.isBreakdown)
@@ -33,11 +46,20 @@ public class NervousBreakdownState : StateBase
     //신경 폭주중 배터리 0이 될시 아이템 효과 종료 시키는 함수
     private IEnumerator BreakdownEndCheck()
     {
-        PlayerController playerController = (PlayerController)characterController;//플레이어 컨트롤러 가져오기
         while (playerController.NowBattery > 0)
         {
             yield return null;
         }
         StartCoroutine(StateFuntion());//신경 폭주 종료
+    }
+
+    //신경 폭주 강제 종료 함수
+    private void StopBreakdown()
+    {
+        if (playerController != null)
+        {
+            if (playerController.isBreakdown)
+                StartCoroutine(StateFuntion());//신경 폭주 종료
+        }
     }
 }
--- Assets/Script/UI/InGameUI/HUD/BossHUDController.cs
+++ Assets/Script/UI/InGameUI/HUD/BossHUDController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHUDController : MonoBehaviour
+public class BossHUDController : MonoBehaviour
 {
     private CharacterController _characterController;//캐릭터 컨트롤러
-    public Image hpBar;//플레이어 체력바 
+    public GameObject hpBarPanel;//hp바 표현 패널
+    public Image hpBar;//보스 체력바 
     private float nowHp = 0;//현재 체력
     public float fillSpeed = 10;//체력바 변경 시 체우는 속도
     public float minHpFillAmount = 0.05f;//최소 HP바 크기
@@ -15,29 +16,34 @@ public class PlayerHUDController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _characterController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();//캐릭터 컨트롤러 초기화
-        //nowHp = _characterController.NowHp;//현재 체력 정보 초기화
+        GameObject bossObj = GameObject.FindGameObjectWithTag("Boss");
+        if (bossObj != null)
+            _characterController = bossObj.GetComponent<CharacterController>();//캐릭터 매니저 초기화
+        else
+            hpBarPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((_characterController))
+        if ((_characterController != null))
         {
             float targetHp = _characterController.NowHp;//목표 체력
             //체력 변동 시 체력을 점진적으로 변화 시키는 코루틴 실행
-            if (nowHp != targetHp && runningCoroutine ==  null)
+            if (nowHp != targetHp && runningCoroutine == null)
+            {
                 runningCoroutine = StartCoroutine(IncreaseHpGauge(targetHp, fillSpeed));
+            }
         }
-        
     }
 
     //플레이어 체력바를 목표 체력 값을 점진적으로 변화시키는 코루틴
-    IEnumerator IncreaseHpGauge(float targetHp,  float fillSpeed)
+    IEnumerator IncreaseHpGauge(float targetHp, float fillSpeed)
     {
         float fillAmount = 0;
         float maxHp = _characterController._characterStatus.maxHp;//최대 체력 가져오기
         float startHp = nowHp;
+        
         while (true)
         {
             //조건 채크
@@ -45,14 +51,14 @@ public class PlayerHUDController : MonoBehaviour
                 break;
             nowHp += nowHp > targetHp ? -fillSpeed : fillSpeed;//현재 체력 갱신
             fillAmount = nowHp / maxHp;//최대체력바 비율 계산
-            hpBar.fillAmount  = fillAmount >= minHpFillAmount || nowHp == 0 ? fillAmount : minHpFillAmount;//최대체력바 갱신
+
+            hpBar.fillAmount = fillAmount >= minHpFillAmount || nowHp == 0 ? fillAmount : minHpFillAmount;//최대체력바 갱신
             yield return new WaitForFixedUpdate();//증가  딜레이 폭
         }
         //체력바 재조정
         nowHp = targetHp;
         fillAmount = nowHp / maxHp;//최대체력바 비율 계산
         hpBar.fillAmount = fillAmount >= minHpFillAmount || nowHp == 0 ? fillAmount : minHpFillAmount;//최대체력바 갱신
-
         runningCoroutine = null;
     }
 }
