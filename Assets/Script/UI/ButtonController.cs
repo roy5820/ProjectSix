00279b6bd46dd2f29a1d580ca76f7d792dbe8ce1
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-06-05 00:08:09 +0900

    UI 수정

--- Assets/Script/UI/ButtonController.cs
+++ Assets/Script/UI/ButtonController.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
+    public float popupMoveSeed = 0.1f;
+    private Coroutine moveCorutine = null;
     //씬이동 처리
     public void OnSceneMove(string sceneName)
     {
@@ -25,6 +27,35 @@ public class ButtonController : MonoBehaviour
     public void OnClosePopupPanel(GameObject gameObject)
     {
         gameObject.SetActive(false);
+    }
+
+    //팝업 Up
+    public void OnUpPopupPanel(GameObject gameObject)
+    {
+        if(moveCorutine == null)
+            moveCorutine = StartCoroutine(UpDownPopup(gameObject, 0.5f, popupMoveSeed));
+    }
+
+    //팝업 Down
+    public void OnDownPopupPanel(GameObject gameObject)
+    {
+        if (moveCorutine == null)
+            moveCorutine = StartCoroutine(UpDownPopup(gameObject, 1.5f, popupMoveSeed));
+    }
+
+    //팝업창 올리고 내리는 기능 구현을 하는 코루틴 함수
+    private IEnumerator UpDownPopup(GameObject popupObject, float targetY, float moveSpeed)
+    {
+        RectTransform rectTransform = popupObject.GetComponent<RectTransform>();
+        float nowY = rectTransform.pivot.y;
+        
+        while (nowY != targetY)
+        {
+            yield return new WaitForFixedUpdate();
+            nowY = nowY > targetY ? Mathf.Round((nowY - moveSpeed)*10)* 0.1f : Mathf.Round((nowY + moveSpeed) * 10) * 0.1f;
+            popupObject.GetComponent<RectTransform>().pivot = new Vector2(rectTransform.pivot.x, nowY);
+        }
+        moveCorutine = null;
     }
 
     //인게임 종료 시 게임메니저 제거를 위한 함수
---
+++ Assets/Script/UI/InGameUI/HUD/TurnSteelTImer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSteelTImer : MonoBehaviour
+{
+    public GameObject rewardObj = null;//보상 표시 오브젝트
+    public GameObject TurnSteelTimer = null;
+
+    
+}
