63df648f41336af7c67f974f5404a42df9cffc51
roy5820 <troy5830@gmail.com>
2024-06-14 10:29:06 +0900

    왠지 모르게 추가된 코드 삭제

--- Assets/Script/StatePattern/CharacterState.cs
+++ Assets/Script/StatePattern/CharacterState.cs
@@ -1,8 +1,5 @@
 
 public interface CharacterState
 {
-    CharacterStateContext CharacterStateContext { get; set; }
-    CharacterStateContext CharacterStateContext1 { get; set; }
-
     void Handle(CharacterController controller, params object[] datas);
 }
